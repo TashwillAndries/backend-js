c8090372d9481d3c9a300eaa7faa3eaa654520e8
TashwillAndries <tashwilla27@gmail.com>
2021-08-16 19:45:19 +0200

    new updates

--- app.py
+++ app.py
@@ -190,8 +190,8 @@ def admin_registration():
     response = {}
     db = Database()
     if request.method == "POST":
-        username = request.form['admin_username']
-        password = request.form['admin_password']
+        username = request.json['admin_username']
+        password = request.json['admin_password']
 
         query = "INSERT INTO admin (admin_username,admin_password) VALUES(?,?)"
         values = (username, password)
